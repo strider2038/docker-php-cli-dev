19e76199b06df02367e5e201ea2a59a2142c2beb
Igor Lazarev <strider2038@yandex.ru>
2021-08-11 17:58:53 +0300

    migrate to github actions

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Deploying image to DockerHub..."
-
-docker login -u "$DOCKER_LOGIN" -p "$DOCKER_PASSWORD";
-docker push strider2038/php-cli-dev
-docker push strider2038/php-cli-dev:7.4
-docker push strider2038/php-cli-dev:7.3
-docker push strider2038/php-cli-dev:7.2
